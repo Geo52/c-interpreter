93d37cca80f643ad914d80a3fe30532adf109266
Jorge Rios <jorgeluisrios47@gmail.com>
2024-12-21 09:48:32 -0600

    added a welcome msg and  a way for the user to input commands

--- main.c
+++ main.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    
+// functions
+int startREPL()
+{
+    char userInput[100];
+
+    while (1)
+    {
+        printf(">> ");
+        // exits if user presses ctrl + D
+        if (fgets(userInput, sizeof(userInput), stdin) == NULL)
+        {
+            return 1;
+        }
+    }
+}
+
+int main()
+{
+    char *user = getenv("USER");
+
+    printf("Hello %s! Welcome to the monkey programming language.\n", user);
+    printf("Feel free to type in commands\n");
+
+    startREPL();
 }
